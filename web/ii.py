d3546dd83bbebd9f3fcb64d7ba0c2cafccb0c47b
NunuInayatunArifah <234110601089@mhs.uinsaizu.ac.id>
2024-12-05 23:41:43 +0700

    commit

---
+++ web/ii.py
@@ -0,0 +1,9 @@
+# Input dari pengguna
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+# Proses penjumlahan
+hasil = angka1 + angka2
+
+# Output hasil
+print(f"Hasil penjumlahan {angka1} + {angka2} = {hasil}")
